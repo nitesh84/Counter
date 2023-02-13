cdb0735520647ebcc7434c6676a385707af270fa
Niesh Wadhwani <niteshwadhwani2019@gmail.com>
2023-02-13 21:04:25 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './Css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(){
+    super()
+    this.state={
+      count:0,
+    }
+  }
+
+
+
+  render() {
+    return (
+      <>
+        <h1>React Web</h1>
+        <p>{this.state.count}</p>
+
+        <button onClick={()=>{
+          this.setState({
+            count:this.state.count-1
+          })
+        }}  >Decrement</button>      
+
+        <button onClick={()=>{
+          this.setState({
+            count:this.state.count+1
+          })
+        }} >Increment</button>    
+        <br />
+        <button onClick={()=>{
+          this.setState({
+            count:0
+          })
+        }} >Reset</button>        
+      </>
+    )
+  }
 }
-
-export default App;
